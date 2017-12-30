cfad8b8788df0fe19d61c1a5b6a85ebcf4477313
mjones2620 <mjones2620@gmail.com>
2017-12-29 21:41:46 -0500

    Updated images and content

--- HMTCore20/wwwroot/js/clientApp/vue-components/contact/contact.js
+++ HMTCore20/wwwroot/js/clientApp/vue-components/contact/contact.js
@@ -1,5 +1,7 @@
 ﻿define(function (require) {
     var Vue = require('vue/vue');
+    var VueCarousel = require('vue/vue-carousel');
+    Vue.use(VueCarousel);
 
     return Vue.component('contact', {
         template: require('text!./contact.html'),
@@ -7,6 +9,10 @@
                 contentUrl: function (string) {
                     return commonApp.root + string;
                 }
+        },
+        components: {
+            'carousel': VueCarousel.Carousel,
+            'slide': VueCarousel.Slide
         }
     })
 });
